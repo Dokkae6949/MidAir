13022aff80aa45bcf4b1f4a801da2dfcc1258dd2
Dokkae6949 <finnliry@gmail.com>
2024-07-30 23:46:01 +0200

    Add animated rotated player sprites

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends AnimatableBody2D
 
 
+@export var rotated_textures: Array[RotatedTexture]
 @export_group("Movement")
 ## The amount of rotation to the left/right that should be applied
 @export var max_rotation_degrees: float = 45.0
@@ -12,6 +13,7 @@ extends AnimatableBody2D
 @onready var sprite: Sprite2D = $Sprite
 
 var _target_rotation_degrees: float = 0.0
+var _rotation_degrees: float = 0.0
 
 
 func _process(delta: float) -> void:
@@ -24,16 +26,34 @@ func _process(delta: float) -> void:
 	elif allow_rotation_reset:
 		_target_rotation_degrees = 0
 	
-	# TODO: Refactor this to make teleportation smoother
+	_update_sprite()
+
+
+func _physics_process(delta: float) -> void:
+	_rotation_degrees = lerpf(_rotation_degrees, _target_rotation_degrees, rotation_speed * delta)
+	
+	# Movement input depends on how much we have rotated compared to the target rotation
+	var movement_input = _rotation_degrees / max_rotation_degrees
+	position.x += movement_input * movement_speed * delta
+	
+	_handle_bounds()
+
+
+# TODO: Refactor this to make teleportation smoother
+func _handle_bounds() -> void:
 	if position.x < 0.0:
 		position.x = get_viewport_rect().size.x
 	if position.x > get_viewport_rect().size.x:
 		position.x = 0
 
 
-func _physics_process(delta: float) -> void:
-	rotation_degrees = lerpf(rotation_degrees, _target_rotation_degrees, rotation_speed * delta)
-	
-	# Movement input depends on how much we have rotated compared to the target rotation
-	var movement_input = rotation_degrees / max_rotation_degrees
-	position.x += movement_input * movement_speed * delta
+func _update_sprite() -> void:
+	var closest_distance: float = INF
+	var closest: RotatedTexture
+	for rotated_texture in rotated_textures:
+		var distance = abs(rotated_texture.rotation_target - _rotation_degrees)
+		if distance < closest_distance:
+			closest_distance = distance
+			closest = rotated_texture
+	if sprite.texture != closest.texture:
+		sprite.texture = closest.texture
---
+++ scripts/rotated_texture.gd
@@ -0,0 +1,4 @@
+class_name RotatedTexture extends Resource
+
+@export var rotation_target: float
+@export var texture: Texture2D
